767d1507efce2e7ce629b3ab37413211a7e16e8a
Abner Franco <abnerfrancoh@gmail.com>
2024-08-27 17:48:00 -0300

    Mescla dos arquivos de matriz

--- Matriz.h
+++ Matriz.h
@@ -4,26 +4,84 @@
 #include<vector>
 using namespace std;
 
-template <class Type>
+template <class T>
 class Matriz
 { 
     public:
     Matriz();
     Matriz(int l, int c);
-    Matriz(int l, int c, vector<Type> v);
+    Matriz(int l, int c, vector<T> v);
     ~Matriz();
 
-    Type getInfo(int l, int c);
-    Type setInfo(int l, int c, Type val);
-    Type setInfo(int l, int c, vector<Type> v);
+    
+    T setInfo(int l, int c, vector<T> v);
+    T& operator()(int l, int c);
 
     private:
 
     int l, c;
-    vector<Type> v;
-    vector<Type> L;
-    vector<Type> U;
+    vector<T> v;
+    vector<T> L;
+    vector<T> U;
 
 };
+template <class T> 
+Matriz<T>::Matriz()
+{
+    l = 0;
+    c = 0;
+    v = {};
+    L = {};
+    U = {};
+}
+
+template <class T>
+Matriz<T>::Matriz(int l, int c)
+{
+    this->l = l;
+    this->c = c;
+    v.resize(l*c);
+    if(l = c)
+    {
+        L.resize((l*(l-1))/2);
+        U.resize( ((l*(l-1))/2) + l);
+    }
+}
+
+ template <class T>
+ Matriz<T>::Matriz(int l, int c, vector<T> v)
+ {
+     this->l = l;
+     this->c = c;
+     this->v = v;
+     if(l = c)
+     {
+         L.resize((l*(l-1))/2);
+         U.resize( ((l*(l-1))/2) + l);
+     }
+ }
+
+template <class T>
+Matriz<T>::~Matriz()
+{
+    v.clear();
+    L.clear();
+    U.clear();
+}
+
+template <class T>
+T& Matriz<T>::operator()(int l, int c)
+{
+    return v[(l-1)*this->c + c];
+}
+
+ template <class T>
+ T Matriz<T>::setInfo(int l, int c, vector<T> v)
+ {
+     this->v = v;
+     return v[(l-1)*this->c + c];
+ }
+
+
 
 #endif
